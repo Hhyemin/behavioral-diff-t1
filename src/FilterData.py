b103dadaed85f5281f5359aae9d0c7e14e7b1cff
Huimin <huhuimin236@gmail.com>
2024-11-11 17:23:07 +0100

    Further simplify the code.

--- src/FilterData.py
+++ src/FilterData.py
@@ -6,14 +6,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--data", help="The original list or string")
 parser.add_argument("--filter", type=str, help="To filter out 'num' or 'letter'")
 
-def filter_out_nums(data):
-    filtered_data = [char for char in data if not str(char).isdigit()]
-    return filtered_data
-
-def filter_out_letters(data):
-    filtered_data = [char for char in data if not str(char).isalpha()]
-    return filtered_data
-    
+   
 if __name__=="__main__":
     args = parser.parse_args()
     try:
@@ -25,9 +18,9 @@ if __name__=="__main__":
 
     filtered = None
     if filter == "num":
-        filtered = filter_out_nums(data)
+        filtered = [char for char in data if not str(char).isdigit()]
     else:
-        filtered = filter_out_letters(data)
+        filtered = [char for char in data if not str(char).isalpha()]
     
     # Print filtered data in the original type
     reformatted_filtered = ''.join(filtered) if isinstance(data, str) else filtered
